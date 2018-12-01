06e45a7f44b71a7e5932229ac6850bd4d480a2f1
jozech <jose.chavez.alvarez@ucsp.edu.pe>
2018-12-01 17:53:25 -0500

    normalize frame and threshold by mean pixels

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <stdio.h>
 #include <stdlib.h>
-
+//#include <time.h>
+#include <unistd.h>
 #include <iostream>
 using namespace cv;
 using namespace std;
@@ -12,12 +13,18 @@ int max_thresh  = 255;
 
 Mat frame;
 Mat gray;
+Mat normImg;
 
-void findCenters(int, int);
-        
+void   findCenters(int, int);
+void   transLineal(Mat, Mat&);
+double getMeanValue(Mat );
+
 int main(int argc, char** argv)
 {
-    VideoCapture cap("files/calibration_kinectv2.avi");
+    
+    string filename = argv[1];
+    
+    VideoCapture cap("files/"+filename);
 
     if (!cap.isOpened())
     {
@@ -29,20 +36,29 @@ int main(int argc, char** argv)
     Mat rot;
     Mat dst;
     Mat dst2;
-
+    Mat imgT;
+    Mat frameHSV;
     for(;;)
     {
-
+        //usleep(10000);
         cap >> frame;
         if(frame.empty())
             break;
 
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-        blur( gray, gray, Size(3,3) );
-        //GaussianBlur(gray, gray, Size(7,7), 1.5, 1.5);
+        transLineal(frame, imgT);
+        //imshow("img TranLineal", imgT);
+        cvtColor(imgT, gray, COLOR_BGR2GRAY);
+        
+        // v1 : -30
+        // v2 : +10
+        double thresh_val = getMeanValue(gray) + 10;
+
+        std::cout << "thresh_val : " << thresh_val << std::endl; 
+        //blur( gray, gray, Size(3,3) );
+        GaussianBlur(gray, gray, Size(3,3), 0, 0);
 
         char k = waitKey(1);
-        findCenters(thresh, max_thresh);
+        findCenters((int)thresh_val, max_thresh);
         if (k == 27) 
             break;
     }
@@ -57,7 +73,11 @@ void findCenters(int thresh, int max_thresh)
     vector<Vec4i> hierarchy;
 
     threshold( gray, threshold_output, thresh, max_thresh, THRESH_BINARY );
-    //imshow("threshold", threshold_output);
+    Mat element = getStructuringElement( MORPH_ELLIPSE,
+                                       Size( 3, 3),
+                                       Point( 1, 1 ) );
+    erode( threshold_output, threshold_output, element );
+    imshow("threshold", threshold_output);
 
     findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0));
     vector<Point> centers(contours.size());
@@ -68,9 +88,9 @@ void findCenters(int thresh, int max_thresh)
     int counte = 0;
     for( int i = 0; i < contours.size(); i++ )
     {
-        if( contours[i].size() > 26  && contours[i].size() < 150)
+        if( contours[i].size() > 4  && contours[i].size() < 150)
         {
-            minRect[counte] = minAreaRect( Mat(contours[i]) );
+            minRect[counte]    = minAreaRect( Mat(contours[i]) );
             minEllipse[counte] = fitEllipse( Mat(contours[i]) );
             counte++;
         }
@@ -85,7 +105,7 @@ void findCenters(int thresh, int max_thresh)
     int errormax = 3; 
 
     Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-    
+
     for( int i = 0; i< counte; i++ )
     {
         color = Scalar( 255, 255, 0);
@@ -118,4 +138,52 @@ void findCenters(int thresh, int max_thresh)
         }
     }
     imshow("foo", frame);
+}
+
+void transLineal(Mat inputImg,Mat& imgT)
+{
+    //Mat ch1, ch2, ch3;
+    inputImg.convertTo(inputImg, CV_32FC1);
+    Mat channels[3];
+    Mat sum;
+
+    double min, max;
+    split(inputImg, channels);
+
+    sum = channels[0] + channels[1] + channels[2];
+
+    minMaxLoc(sum, &min, &max);
+    //std::cout <<  max << std::endl;
+
+    //std::cout << "sum : " << sum.size() << std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        //std::cout << "i :" << i <<" channels : " << channels[i].size() << std::endl;
+        //channels[i] /= sum;
+        //channels[i] *= 255;
+        
+        //std::cout << "i :" << i <<" channels : " << channels[i].size() << std::endl;
+        minMaxLoc(channels[i], &min, &max);
+        //std::cout <<  max << std::endl;
+        channels[i] = 255 * (channels[i] - min)/ (max - min);
+        channels[i].convertTo(channels[i], CV_8U);
+        //normalize(channels[i], channels[i], min, max, NORM_MINMAX);
+    }
+    merge(channels,3,imgT);
+    
+}
+
+double getMeanValue(Mat grayInp)
+{
+    float sumV = 0.0;
+    float size = grayInp.rows * grayInp.cols;
+    for (int i = 0; i < grayInp.rows; i++)
+    {
+        for(int j = 0; j < grayInp.cols; j++)
+        {
+            sumV += grayInp.at<uint8_t>(i,j)/size;
+        }
+    }
+   
+    return sumV;
 }
