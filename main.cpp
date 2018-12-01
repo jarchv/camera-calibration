6195f76ea64be10d5b4563d044827bf693bf4ad2
LuisErnestoColchado <agoraphobiacroz@gmail.com>
2018-12-01 02:23:01 -0500

    centers and radius distance

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ Mat gray;
 RNG rng(12345);
 
 void thresh_callback(int, void* );
-
+        Mat frame;
 int main(int argc, char** argv)
 {
     VideoCapture cap("files/calibration_kinectv2.avi");
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
 
     for(;;)
     {
-        Mat frame;
+
         cap >> frame;
         if(frame.empty())
             break;
@@ -70,7 +70,7 @@ int main(int argc, char** argv)
         */
         //resize(dst, dst2, cv::Size(dst.cols*0.5, dst.rows*0.5));
         char k = waitKey(1);
-        imshow("foo", frame);
+        //imshow("foo", frame);
         createTrackbar( " Threshold:", "foo", &thresh, max_thresh, thresh_callback );
         thresh_callback( 0, 0 );
         if (k == 27) 
@@ -89,39 +89,51 @@ void thresh_callback(int, void* )
     /// Detect edges using Threshold
     threshold( gray, threshold_output, thresh, 255, THRESH_BINARY );
 
-    /// Find contours
-    imshow("threshold", threshold_output);
+    /// Find contours 
+    //imshow("threshold", threshold_output);
 
     findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0));
-
+    vector<Point> centers(contours.size());
+    vector<float> radius(contours.size());
     /// Find the rotated rectangles and ellipses for each contour
     vector<RotatedRect> minRect( contours.size() );
     vector<RotatedRect> minEllipse( contours.size() );
-
+    int counte = 0;
     for( int i = 0; i < contours.size(); i++ )
     {
-        minRect[i] = minAreaRect( Mat(contours[i]) );
-        if( contours[i].size() > 5 )
+        //minRect[i] = minAreaRect( Mat(contours[i]) );
+        if( contours[i].size() > 26  && contours[i].size() < 150)
         {
-            minEllipse[i] = fitEllipse( Mat(contours[i]) );
+            minRect[counte] = minAreaRect( Mat(contours[i]) );
+            minEllipse[counte] = fitEllipse( Mat(contours[i]) );
+            counte++;
         }
     }
 
+    Scalar color;
+    Point2f rect_points[4];
+    int centerx;
+    int centery;
+    int radiustemp;
+    int errormaxradio = 7;
+    int errormax = 3; 
   /// Draw contours + rotated rects + ellipses
     Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-    for( int i = 0; i< contours.size(); i++ )
+    for( int i = 0; i< counte; i++ )
     {
         //Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        Scalar color = Scalar( 255, 255, 0);
+        color = Scalar( 255, 255, 0);
         // contour
         //drawContours( drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
         // ellipse
         //ellipse( drawing, minEllipse[i], color, 2, 8 );
         // rotated rectangle
-        Point2f rect_points[4]; 
         minRect[i].points( rect_points );
-        
-        int dsum[4];
+        centerx = 0.5*(rect_points[0].x + rect_points[2].x);
+        centery = 0.5*(rect_points[0].y + rect_points[2].y);
+        centers[i] = Point(centerx,centery); 
+        radius[i] =  sqrt((rect_points[2].x - rect_points[0].x)*(rect_points[2].x - rect_points[0].x)+(rect_points[2].y - rect_points[0].y)*(rect_points[2].y - rect_points[0].y));
+        /*int dsum[4];
         for (int j = 0; j < 4; j++)
         {
             int x2 = (rect_points[j].x - rect_points[(j + 2)%4].x) * (rect_points[j].x - rect_points[(j + 2)%4].x);
@@ -140,11 +152,41 @@ void thresh_callback(int, void* )
             ellipse( drawing, minEllipse[i], color, 1.2, 8);
             //for( int j = 0; j < 4; j++ )
             //    line( drawing, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
+        }*/
+    }
+        //std::cout << "tam " << contours.size() << std::endl;
+    for(size_t p = 0;p<counte;p++){
+        //std::cout << p << std::endl;
+        //circle(drawing, centers[p], 10, Scalar(255,0,255), 3, LINE_AA);
+        minRect[p].points( rect_points );
+        centerx = 0.5*(rect_points[0].x + rect_points[2].x);
+        centery = 0.5*(rect_points[0].y + rect_points[2].y);
+        int count = 0;
+        radiustemp = sqrt(pow((rect_points[2].x - rect_points[0].x),2)+pow((rect_points[2].y - rect_points[0].y),2));
+        for(size_t k = 0;k<counte;k++)
+        {
+            if(k != p){
+                float dist = sqrt((centerx - centers[k].x)*(centerx - centers[k].x) + (centery - centers[k].y)*(centery - centers[k].y));
+                //std::cout << dist << std::endl;
+
+                if((dist <= errormax) && (abs(radiustemp - radius[k])>errormaxradio))
+                {
+                    //ellipse( drawing, minEllipse[p], color, 1.2, 8);
+                    count++;
+                
+                }
+                if(count > 0){
+
+                    ellipse( frame, minEllipse[p], color, 2, 8);
+                    std::cout << p << " - " << counte << std::endl;
+                    break;
+                
+                } 
+            }
         }
     }
-    
   /// Show in a window
-    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-    imshow( "Contours", drawing );
-
+    //namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    //imshow( "Contours", drawing );
+    imshow("foo", frame);
 }
