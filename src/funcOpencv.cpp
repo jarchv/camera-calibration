fbe92b69f6a81a4fc963b31e8ad3c6591770d950
LuisErnestoColchado <agoraphobiacroz@gmail.com>
2019-01-10 02:29:45 -0500

    calibration chees and circles symm

--- src/funcOpencv.cpp
+++ src/funcOpencv.cpp
@@ -1,6 +1,10 @@
 #include "utils.h"
 #include "base.h"
 
+std::vector<cv::Mat> arrayImageChess;
+std::vector<cv::Mat> arrayImageCircleSym;
+std::vector<cv::Mat> arrayImageCircleAsym;
+
 cv::Mat frameChess;
 cv::Size sizeFrameChess;
 
@@ -8,6 +12,7 @@ cv::Mat frameSym;
 cv::Size sizeFrameSym;
 
 cv::Mat frameAsym;
+cv::Size sizeFrameAsym;
 
 cv::Mat result_chess;
 cv::Mat result_Asym;
@@ -55,7 +60,7 @@ cv::Mat distCoeffsCircleSym;
 std::vector<cv::Mat> rvecsCircleSym;
 std::vector<cv::Mat> tvecsCircleSym;
 
-cv::Size board_sz_asym;
+cv::Size board_sz_Asym;
 std::vector<cv::Point2f> patternCircleAsym;
 std::vector<std::vector<cv::Point2f>> arrayPatternCircleAsym;
 std::vector<cv::Point3f> objCircleAsym;
@@ -73,12 +78,23 @@ cv::Mat  circleSymmetric(cv::Mat frameSym,clock_t begin_time_circleSym);
 
 std::vector<cv::Vec3f> circles;
 
+
+int maxFrame = 30;
 int main(int argc, char** argv)
 {
     //std::string filename = argv[1];
     cv::VideoCapture cap1("../files/board.avi");
     cv::VideoCapture cap2("../files/circlesAsym.avi");
     cv::VideoCapture cap3("../files/circlesSym.avi");
+    for(int i = 1; i <= 27;i++){
+        arrayImageChess.push_back(cv::imread("../files/ajedrez/tmp" + std::to_string(i) + ".png" ));
+    }
+    for(int i = 1; i <= 15;i++){
+        arrayImageCircleSym.push_back(cv::imread("../files/sm_circles/tmp" + std::to_string(i) + ".png" ));
+    }
+    /*for(int i = 1; i <= 28;i++){
+        arrayImageCircleAsym.push_back(cv::imread("../files/asm_cicles/tmp" + std::to_string(i) + ".png" ));
+    }*/
 
     if (!cap1.isOpened())
     {
@@ -96,65 +112,91 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    int numCornersHor = 8;
+    //begin TO DO 
+    int numCornersHor = 9;
     int numCornersVer = 6;
     int numSquares = numCornersHor * numCornersVer;
     board_sz = cv::Size(numCornersHor, numCornersVer);
     for(int j=0;j<numSquares;j++)
-        objChess.push_back(cv::Point3f(j/numCornersHor, j%numCornersHor, 0.0f));
+        objChess.push_back(cv::Point3f((j/numCornersHor)*48.0, (j%numCornersHor)*48.0, 0.0f));
 
     int numCornersHor_Sym = 7;
     int numCornersVer_Sym = 5;
     int numCircles = numCornersHor_Sym * numCornersVer_Sym;
     board_sz_sym = cv::Size(numCornersHor_Sym, numCornersVer_Sym);
-    for(int j=0;j<numCircles;j++)
-        objCircleSym.push_back(cv::Point3f(j/numCornersHor_Sym, j%numCornersHor_Sym, 0.0f));
+    float distanceSym = 35;
+    for( int i = 0; i < board_sz_sym.height; ++i )
+        for( int j = 0; j < board_sz_sym.width; ++j )
+           objCircleSym.push_back(cv::Point3f(float( j*distanceSym ), float( i*distanceSym ), 0));
+    //for(int j=0;j<numCircles;j++)
+    //    objCircleSym.push_back(cv::Point3f(j/numCornersHor_Sym, j%numCornersHor_Sym, 0.0f));
 
-    int numCornersHor_Asym = 7;
-    int numCornersVer_Asym = 5;
+    int numCornersHor_Asym = 5;
+    int numCornersVer_Asym = 14;
     int numCirclesAsym = numCornersHor_Asym * numCornersVer_Asym;
-    board_sz_sym = cv::Size(numCornersHor_Asym, numCornersVer_Asym);
-    for(int j=0;j<numCirclesAsym;j++)
-        objCircleAsym.push_back(cv::Point3f(j/numCornersHor_Asym, j%numCornersHor_Asym, 0.0f));
-
+    board_sz_Asym = cv::Size(numCornersHor_Asym, numCornersVer_Asym);
+    //for(int j=0;j<numCirclesAsym;j++)
+    //    objCircleAsym.push_back(cv::Point3f(j/numCornersHor_Asym, j%numCornersHor_Asym, 0.0f));
+    float distanceAsym = 18;
+    for( int i = 0; i < board_sz_Asym.height; i++ )
+        for( int j = 0; j < board_sz_Asym.width; j++ )
+            objCircleAsym.push_back(cv::Point3f(float((2*j + i % 2)*distanceAsym), float(i*distanceAsym), 0));
+    //end TO DO 
+    //int j =0;
+    int countFrame =0 ;
     for(;;)
     {
-        cap1 >> frameChess;
-        if(frameChess.empty())
-            break;
+        //cap1 >> frameChess;
+        //if(frameChess.empty())
+        //    break;
             
-        cap2 >> frameAsym;
+        //cap2 >> frameAsym;
         cap3 >> frameSym;
 
         begin_time_chess = clock();
         begin_time_circleAsym = clock();
         begin_time_circleSym = clock();
+        if(countFrame < 27){
+            result_chess = chessboar(arrayImageChess[countFrame],begin_time_chess);
+            cv::imshow("chess",result_chess);
+        }
 
-        result_chess = chessboar(frameChess,begin_time_chess);
-        //result_Asym = circleAsymetric(frameAsym,begin_time_circleAsym);
+        if(frameSym.empty())
+            break;
+        //result_Asym = circleAsymmetric(arrayImageCircleAsym[j],begin_time_circleAsym);
         result_Sym = circleSymmetric(frameSym,begin_time_circleSym);
-        cv::imshow("chess",result_chess);
+        //cv::imshow("chess",result_chess);
         //cv::imshow("asy",result_Asym);
         cv::imshow("sym",result_Sym);
+        
         //times 
         //time_elapsed_Asym
         //time_elapsed_Sym
         //time_elapsed_chess
+        countFrame++;
         char k = cv::waitKey(1);
         if ( k == 27) 
             break;
+        
     }
 
     cap1.release();
+    std::cout << "Result Chessboard" << std::endl;
     errorChess = cv::calibrateCamera(arrayObjChess, arrayPatternChess, sizeFrameChess,intrinsicChess, distCoeffsChess, rvecsChess, tvecsChes);
     std::cout << "error " << errorChess/arrayObjChess.size() << std::endl;
     std::cout << "Intrinsic " << intrinsicChess << std::endl;
     
     cap2.release();
+    std::cout << "Result Symmetric Circles" << std::endl;
     errorCiclesSym = cv::calibrateCamera(arrayObjCircleSym, arrayPatternCircleSym, sizeFrameSym,intrinsicCircleSym, distCoeffsCircleSym, rvecsCircleSym, tvecsCircleSym);
     std::cout << "error " << errorCiclesSym/arrayObjCircleSym.size() << std::endl;
     std::cout << "Intrinsic " << intrinsicCircleSym << std::endl;
+
     cap3.release();
+    /*errorCiclesAsym = cv::calibrateCamera(arrayObjCircleAsym, arrayPatternCircleAsym, sizeFrameAsym,intrinsicCircleAsym, distCoeffsCircleAsym, rvecsCircleAsym, tvecsCircleAsym);
+    std::cout << "error " << errorCiclesAsym/arrayObjCircleAsym.size() << std::endl;
+    std::cout << "Intrinsic " << intrinsicCircleAsym << std::endl;
+    cv::imshow("intrinsic",intrinsicCircleAsym);*/
     return 0;
 }
 
@@ -171,6 +213,7 @@ cv::Mat chessboar(cv::Mat frameChess,clock_t begin_time_chess){
         arrayPatternChess.push_back(patternChess);
         arrayObjChess.push_back(objChess);
         sizeFrameChess = frameChess.size();
+        
     }
 
     float temp_time_chess = float( clock () - begin_time_chess ) /  CLOCKS_PER_SEC;
@@ -182,13 +225,14 @@ cv::Mat chessboar(cv::Mat frameChess,clock_t begin_time_chess){
 
 cv::Mat circleAsymmetric(cv::Mat frameAsym,clock_t begin_time_circleAsym){
     cv::cvtColor(frameAsym, grayAsym, cv::COLOR_BGR2GRAY);
-    bool found = findCirclesGrid( frameAsym, board_sz_sym, patternCircleSym);
+    bool found = findCirclesGrid( frameAsym, board_sz_Asym, patternCircleAsym,cv::CALIB_CB_ASYMMETRIC_GRID);
     if(found){
         //cv::cornerSubPix(graySym, patternCircleSym, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.001));
-        cv::drawChessboardCorners( frameSym, board_sz_sym, cv::Mat(patternCircleSym), found );
-        arrayPatternCircleSym.push_back(patternCircleSym);
-        sizeFrameSym = frameSym.size();
-        arrayObjCircleSym.push_back(objCircleSym);
+        cv::drawChessboardCorners( frameAsym, board_sz_Asym, cv::Mat(patternCircleAsym), found );
+        arrayPatternCircleAsym.push_back(patternCircleAsym);
+        sizeFrameAsym = frameAsym.size();
+        arrayObjCircleAsym.push_back(objCircleAsym);
+        std::cout << count_frame_Asym << std::endl;
     }
     /*cv::HoughCircles(grayAsym,circles,CV_HOUGH_GRADIENT,1,grayAsym.rows/16,100,30,1,30);
 
@@ -210,12 +254,13 @@ cv::Mat circleAsymmetric(cv::Mat frameAsym,clock_t begin_time_circleAsym){
 cv::Mat  circleSymmetric(cv::Mat frameSym,clock_t begin_time_circleSym){
     cv::cvtColor(frameSym, graySym, cv::COLOR_BGR2GRAY);
     bool found = findCirclesGrid( frameSym, board_sz_sym, patternCircleSym);
-    if(found){
+    if(found && count_frame_Sym % 12 == 0){
         //cv::cornerSubPix(graySym, patternCircleSym, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.001));
         cv::drawChessboardCorners( frameSym, board_sz_sym, cv::Mat(patternCircleSym), found );
         arrayPatternCircleSym.push_back(patternCircleSym);
         sizeFrameSym = frameSym.size();
         arrayObjCircleSym.push_back(objCircleSym);
+        std::cout << count_frame_Sym << std::endl;
     }
 
     float temp_time_Sym = float( clock () - begin_time_circleSym ) /  CLOCKS_PER_SEC;
